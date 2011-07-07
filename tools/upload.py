76199dfb1b43a87f1a760dc60885923467990310
Sam King <samking@google.com>
2011-07-07 15:11:36 -0700

    credit Person Finder for common.sh, rietveld, and upload.py.  Also, python2.5 changed to python

--- tools/upload.py
+++ tools/upload.py
@@ -30,6 +30,7 @@ against by using the '--rev' option.
 """
 # This code is derived from appcfg.py in the App Engine SDK (open source),
 # and from ASPN recipe #146306.
+# Copied from Google Person Finder: http://code.google.com/p/googlepersonfinder
 
 import ConfigParser
 import cookielib
