b35216d994cfd354952ae00bc01b21457507a302
Sam King <samking@google.com>
2011-07-13 13:15:16 -0700

    Added fine-grained control over what to print in the printer class

--- pfif_validator/error_printer.py
+++ pfif_validator/error_printer.py
@@ -17,14 +17,12 @@ __author__ = 'samking@google.com (Sam King)'
 
 """Provides a unified interface for collecting and printing errors."""
 
-# TODO(samking): get xml line number.  errors v warnings.  record_id.  full
-# element text.
-
 class Error:
   """A container for information about an error or warning"""
 
   def __init__(self, message, is_error=True, xml_line_number=None,
-               xml_element_text=None, person_record_id="", note_record_id=""):
+               xml_element_text=None, person_record_id=None,
+               note_record_id=None):
     self.message = message
     self.is_error = is_error
     self.xml_line_number = xml_line_number
@@ -41,21 +39,40 @@ class ErrorPrinter:
   print_error_messages call will print as much as set_printing_options
   specifies."""
 
-  def __init__(self, print_output=True):
+  def __init__(self):
     self.error_messages = {}
     self.test_name = None
-    self.print_output = print_output
+    self.print_properties = {"output" : True, "errors" : True,
+                             "warnings" : True, "xml_line_numbers" : True,
+                             "xml_text" : False, "record_ids" : True}
 
-  def set_printing_options(self, print_output=None):
-    """Turns printing on if print_output is True"""
-    if print_output != None:
-      self.print_output = print_output
+  def set_printing_options(self, output=None, errors=None, warnings=None,
+                           xml_line_numbers=None, xml_text=None,
+                           record_ids=None):
+    """Changes printing options for any specified options.
 
-  @staticmethod
-  def print_arr(arr):
-    """Prints out each elem in the arr on its own line"""
-    for elem in arr:
-      print elem
+    output: if False, will print nothing
+    errors: if False, will not print any Error for which is_error is true
+    warnings: if False, will not print any Error for which is_error is
+        false
+    xml_line_numbers: if True, will print the source line in the XML where
+        the error occurred
+    xml_text: if True, will print the text element from the XML where the
+        error occurred
+    record_ids: if True, will print the note_record_id and/or
+        person_record_id that caused the error"""
+    if output != None:
+      self.print_properties["output"] = output
+    if errors != None:
+      self.print_properties["errors"] = errors
+    if warnings != None:
+      self.print_properties["warnings"] = warnings
+    if xml_line_numbers != None:
+      self.print_properties["xml_line_numbers"] = xml_line_numbers
+    if xml_text != None:
+      self.print_properties["xml_text"] = xml_text
+    if record_ids != None:
+      self.print_properties["record_ids"] = record_ids
 
   def set_current_test(self, test_name):
     """Sets the current test, for the purposes of printing and error messages,
@@ -65,29 +82,51 @@ class ErrorPrinter:
       del self.error_messages[test_name]
 
   def get_errors(self):
-    """Returns a list of all errors for the specified test."""
+    """Returns a list of all errors for the current test."""
     assert self.test_name
     if self.test_name in self.error_messages:
       return self.error_messages[self.test_name]
     else:
       return []
 
-  #TODO(samking): allow fine grained error printing
-  def print_errors(self):
-    """Prints out all errors from a given test, per the options in
-    set_printing_options"""
-    assert self.test_name
-    if self.print_output:
-      print "****" + self.test_name + "****"
-      if self.test_name in self.error_messages:
-        self.print_arr(self.error_messages[self.test_name])
-      print
-
   def add_error(self, error):
-    """Adds a message to the error message list"""
+    """Adds an error to the list"""
     assert self.test_name
     if self.test_name not in self.error_messages:
       test_error_list = []
       self.error_messages[self.test_name] = test_error_list
     test_error_list = self.error_messages[self.test_name]
     test_error_list.append(error)
+
+  def print_errors(self):
+    """Prints out all errors from the current test, per the options in
+    set_printing_options"""
+    assert self.test_name
+    if self.print_properties["output"]:
+      print "****" + self.test_name + "****"
+      if self.test_name in self.error_messages:
+        errors = self.error_messages[self.test_name]
+        for error in errors:
+          if (error.is_error and self.print_properties["errors"]) or (
+              not error.is_error and self.print_properties["warnings"]):
+            message = ""
+            if error.is_error:
+              message += "ERROR: "
+            else:
+              message += "WARNING: "
+            message += error.message + " "
+            if (self.print_properties["xml_line_numbers"] and
+                error.xml_line_number != None):
+              message += "XML Line " + str(error.xml_line_number) + ". "
+            if self.print_properties["record_ids"]:
+              if error.person_record_id != None:
+                message += ("The relevant person_record_id is: " +
+                            error.person_record_id)
+              if error.note_record_id != None:
+                message += ("The relevant note_record_id is: " +
+                            error.note_record_id)
+            if self.print_properties["xml_text"] and error.xml_element_text:
+              message += ("The text of the relevant PFIF XML node: " +
+                          error.xml_element_text)
+            print message
+      print
--- tests/test_printing.py
+++ tests/test_printing.py
@@ -26,43 +26,104 @@ from error_printer import ErrorPrinter, Error
 class PrintingTests(unittest.TestCase):
   """Tests the ErrorPrinter class methods"""
 
-  # TODO(samking): update and modularize test to reflect new, fine grained
-  # printing interface
-  def test_printing(self):
-    """print_test_start, print_error_message, and add_error_message should
-    generate messages if printing is turned on and should not crash.
-    get_error_messages should return a list of error messages."""
-    old_stdout = sys.stdout
+  def test_printer_init(self):
+    """Tests that initializing an ErrorPrinter doesn't raise any exceptions and
+    returns a new object"""
+    self.assertTrue(ErrorPrinter())
+
+  def test_error_storage(self):
+    """For a given test, each call to add_error is stored such that get_error
+    will return it in the order added."""
     printer = ErrorPrinter()
+    printer.set_current_test("Test")
+    self.assertEqual(len(printer.get_errors()), 0)
+    printer.add_error(Error("Message"))
+    printer.add_error(Error("More Message"))
+    printer.add_error(Error("Hey"))
+    self.assertEqual(len(printer.get_errors()), 3)
+    self.assertEqual(printer.get_errors()[2].message, "Hey")
 
-    # mock stdout so that we can tell what gets printed
-    printer.set_printing_options(print_output=False)
-    fake_stdout = StringIO.StringIO()
-    sys.stdout = fake_stdout
-
-    # I can't do anything without setting the test name
-    self.assertRaises(Exception, printer.add_error, Error("Error Message"))
+  def test_must_set_current_test(self):
+    """If set_current_test is not called, add_error, get_errors, and
+    print_errors should raise exceptions"""
+    printer = ErrorPrinter()
+    self.assertRaises(Exception, printer.add_error, Error("Message"))
     self.assertRaises(Exception, printer.print_errors)
     self.assertRaises(Exception, printer.get_errors)
 
-    # start out with no errors
-    printer.set_current_test("Printing Test")
-    self.assertEqual(len(printer.get_errors()), 0)
+  def test_resetting_test_deletes_old_data(self):
+    """If set_current_test is called twice with the same test name, any data
+    stored between the first and second call should be deleted"""
+    printer = ErrorPrinter()
+    printer.set_current_test("Test")
+    printer.add_error(Error("Old"))
+    printer.set_current_test("Test")
+    printer.add_error(Error("New"))
+    self.assertEqual(len(printer.get_errors()), 1)
+    self.assertEqual(printer.get_errors()[0].message, "New")
 
-    # I can add errors and get the back
-    printer.add_error(Error("Error Message"))
-    printer.add_error(Error("Error Message", person_record_id="ID1",
-                            note_record_id="ID2"))
-    self.assertEqual(len(printer.get_errors()), 2)
+  def test_print_options(self):
+    """Tests that each of the printing options in set_printing_options changes
+    the behavior of print_errors"""
+    printer = ErrorPrinter()
 
-    # printing doesn't do anything when set_printing is off
+    # mock stdout so that we can tell what gets printed
+    old_stdout = sys.stdout
+    fake_stdout = StringIO.StringIO()
+    sys.stdout = fake_stdout
+
+    # set up the printer with errors
+    printer.set_current_test("Test")
+    printer.add_error(Error( "Message 1", is_error=True, xml_line_number=333,
+                            xml_element_text="Text", person_record_id="Person",
+                            note_record_id="Note"))
+    printer.add_error(Error("Message 2", is_error=False))
+    printer.add_error(Error("Message 3"))
+    printer.set_printing_options(output=False, errors=False, warnings=False,
+                                 xml_line_numbers=False, xml_text=False,
+                                 record_ids=False)
+
+    # output is off, so nothing should print
     printer.print_errors()
-    self.assertTrue(fake_stdout.tell() == 0)
+    self.assertEqual(fake_stdout.tell(), 0)
 
-    # printing does something when set_printing is on
-    printer.set_printing_options(print_output=True)
+    # with output on but errors and warnings off, only test names should print
+    printer.set_printing_options(output=True)
     printer.print_errors()
-    self.assertTrue(fake_stdout.tell() > 0)
+    self.assertEqual(fake_stdout.getvalue().find("Message"), -1)
+    self.assertNotEqual(fake_stdout.getvalue().find("Test"), -1)
+
+    # with only errors on, only errors should print
+    printer.set_printing_options(errors=True)
+    printer.print_errors()
+    self.assertNotEqual(fake_stdout.getvalue().find("Message 1"), -1)
+    self.assertEqual(fake_stdout.getvalue().find("Message 2"), -1)
+    # the default value of is_error should be True, so Message 3 should print
+    self.assertNotEqual(fake_stdout.getvalue().find("Message 3"), -1)
+
+    # with warnings on, warnings should print
+    printer.set_printing_options(warnings=True)
+    printer.print_errors()
+    self.assertNotEqual(fake_stdout.getvalue().find("Message 2"), -1)
+
+    # line numbers, xml text, and record IDs should not print with them off and
+    # should print with them on
+    self.assertEqual(fake_stdout.getvalue().find("333"), -1)
+    printer.set_printing_options(xml_line_numbers=True)
+    printer.print_errors()
+    self.assertNotEqual(fake_stdout.getvalue().find("333"), -1)
+
+    self.assertEqual(fake_stdout.getvalue().find("Text"), -1)
+    printer.set_printing_options(xml_text=True)
+    printer.print_errors()
+    self.assertNotEqual(fake_stdout.getvalue().find("Text"), -1)
+
+    self.assertEqual(fake_stdout.getvalue().find("Person"), -1)
+    self.assertEqual(fake_stdout.getvalue().find("Note"), -1)
+    printer.set_printing_options(record_ids=True)
+    printer.print_errors()
+    self.assertNotEqual(fake_stdout.getvalue().find("Person"), -1)
+    self.assertNotEqual(fake_stdout.getvalue().find("Note"), -1)
 
     sys.stdout = old_stdout
 
