87998726977aac73686e124781bc0713df7e1891
Sam King <samking@cs.stanford.edu>
2011-07-28 15:21:38 -0700

    added test runners from Person Finder

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,8 +16,6 @@
 """Tests for utils.py"""
 
 import sys
-# TODO(samking): remove this after adding a test running script
-sys.path.append(sys.path[0] + '/../pfif_validator')
 import utils
 import unittest
 
