ffc94e16cf6298132f154c7942a1f19e19931791
Sam King <samking@google.com>
2011-08-12 10:31:16 -0700

    Made home_state match real iso-3166-2 states, which can be prefixed with the country.

---
+++ pfif_validator/utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# Copyright 2011 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities for the PFIF Validator"""
+
+import re
+from datetime import datetime
+import urllib
+
+# XML Parsing Utilities
+
+def extract_tag(etree_tag):
+  """An etree tag comes in the form: {namespace}tag.  This returns the tag"""
+  match = re.match(r'(?:\{.+\})?(.+)', etree_tag)
+  if not match:
+    return ""
+  return match.group(1)
+
+# Dependency Injection for Time -- from PersonFinder
+_utcnow_for_test = None # pylint: disable=c0103
+
+def set_utcnow_for_test(now):
+  """Set current time for debug purposes."""
+  global _utcnow_for_test # pylint: disable=w0603
+  _utcnow_for_test = now
+
+def get_utcnow():
+  """Return current time in utc, or debug value if set."""
+  return _utcnow_for_test or datetime.utcnow()
+
+# Dependency injection for files
+_file_for_test = None # pylint: disable=c0103
+
+def set_file_for_test(file_for_test):
+  """Set current file or url for debugging purposes."""
+  global _file_for_test # pylint: disable=w0603
+  _file_for_test = file_for_test
+
+def open_file(filename, mode='r'):
+  """Opens the file or returns a debug value if set."""
+  return _file_for_test or open(filename, mode)
+
+def open_url(url):
+  """Opens the url or returns a debug value if set."""
+  return _file_for_test or urllib.urlopen(url)
