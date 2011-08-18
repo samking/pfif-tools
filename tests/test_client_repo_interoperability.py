4ea687a709d2a37dd451896e822b5c54ef943ad4
Sam King <samking@google.com>
2011-08-18 14:54:41 -0700

    Check 1.1/3.1 works: retrieve a person record

--- tests/test_client_repo_interoperability.py
+++ tests/test_client_repo_interoperability.py
@@ -21,13 +21,14 @@ import utils
 from client_repo_interoperability import ClientTester
 import unittest
 from StringIO import StringIO
+import tests.pfif_xml as PfifXml
 
 class ClientRepoTests(unittest.TestCase):
   """Tests each test function in client_repo_interoperability.py"""
 
   def test_expand_url(self):
     """Verifies that expand_url correctly replaces symbols with data."""
-    tester = ClientTester(api_key='aoeu')
+    tester = ClientTester(api_key='aoeu', initialize_now=False)
     response = tester.expand_url(url='example.org/api?k=$k&p=$p&n=$n',
                                  person_record_id='example.com/p1',
                                  note_record_id='example.net/n1')
@@ -38,19 +39,20 @@ class ClientRepoTests(unittest.TestCase):
   def test_retrieve_person_record(self):
     """check_retrieve_person_record should return messages if and only if the
     repository output differs from the expected output."""
-    tester = ClientTester()
-    tester.generate_data_set()
+    tester = ClientTester(first_person=1, last_person=1,
+                          first_person_with_notes=-1, last_person_with_notes=-2,
+                          first_note=-1, last_note=-2)
 
     # When provided with a record identical to the desired record, there should
     # be no messages
-    utils.set_file_for_test(StringIO(''))
-    messages = tester.test_retrieve_person_record()
+    utils.set_file_for_test(StringIO(PfifXml.XML_TEST_EXAMPLE_PERSON_1))
+    messages = tester.check_retrieve_person_record()
     self.assertEqual(len(messages), 0)
 
     # When provided with a record different from to the desired record, there
     # should be some messages
-    utils.set_file_for_test(StringIO(''))
-    messages = tester.test_retrieve_person_record()
+    utils.set_file_for_test(StringIO(PfifXml.XML_11_FULL))
+    messages = tester.check_retrieve_person_record()
     self.assertTrue(len(messages) > 0)
 
 if __name__ == '__main__':
